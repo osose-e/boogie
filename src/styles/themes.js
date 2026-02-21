270ccfbeabf4f815e22ee21f753409ceea84ae64
Ananya Navale <anavale@stanford.edu>
2026-02-21 09:59:39 -0800

    Added font files and installed expo-font

---
+++ src/styles/themes.js
@@ -0,0 +1,8 @@
+import { colors } from "./colors";
+import { fonts, fontSizes } from "./typography";
+
+export const theme = {
+  colors,
+  fonts,
+  fontSizes,
+};
---
+++ src/styles/typography.js
@@ -0,0 +1,16 @@
+export const fonts = {
+    wordmark: "BagelFatOne",
+    header1: "AlbertSans-Bold",
+    header2: "AlbertSans-Bold",
+    header3: "AlbertSans-Regular",
+    body: "AlbertSans-Regular",
+    bodyItalic: "AlbertSans-Italic",
+}
+
+export const fontSizes = {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24
+}
